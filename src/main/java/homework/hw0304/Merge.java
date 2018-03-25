b8d2d47c797bf16d5788aa15ad87d57ed3209191
Dimon1234 <wap.mmj@mail.ru>
2018-03-25 15:41:45 +0400

    Реализован Merge с двумя условиями в цикле

--- src/main/java/homework/hw0304/Merge.java
+++ src/main/java/homework/hw0304/Merge.java
@@ -4,15 +4,19 @@ public class Merge {
 
     public static void go(long[] array, long[] workspace, int low, int mid, int high) {
         int i = low;
-        int j = mid + 1;
+        int j = high;
 
-        System.arraycopy(array, low, workspace, low, high + 1 - low);
+        for (int k = low; k < mid + 1; k++) {
+            workspace[k] = array[k];
+        }
+        for (int k = mid + 1; k <= high; k++) {
+            workspace[k] = array[high + mid + 1 - k];
+        }
+
 
         for (int k = low; k <= high; k++) {
-            if (i > mid) array[k] = workspace[j++];
-            else if (j > high) array[k] = workspace[i++];
-            else if (workspace[i] < workspace[j]) array[k] = workspace[i++];
-            else array[k] = workspace[j++];
+            if (workspace[i] < workspace[j]) array[k] = workspace[i++];
+            else array[k] = workspace[j--];
         }
 
     }
